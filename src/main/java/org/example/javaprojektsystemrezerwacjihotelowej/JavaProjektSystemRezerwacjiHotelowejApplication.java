0db9ac229db83480c49f21678bf666801dcb5851
Anna <gska.anna.anna@gmail.com>
2025-05-14 21:06:52 +0200

    init blank java project

---
+++ src/main/java/org/example/javaprojektsystemrezerwacjihotelowej/JavaProjektSystemRezerwacjiHotelowejApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaprojektsystemrezerwacjihotelowej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjektSystemRezerwacjiHotelowejApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjektSystemRezerwacjiHotelowejApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaprojektsystemrezerwacjihotelowej/JavaProjektSystemRezerwacjiHotelowejApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaprojektsystemrezerwacjihotelowej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjektSystemRezerwacjiHotelowejApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
