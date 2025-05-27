2b2d481cb23355812fc8288d6a76293d4cb07051
Anna <gska.anna.anna@gmail.com>
2025-05-27 23:59:01 +0200

    Add Swagger UI configuration and OpenAPI dependencies

--- src/main/java/org/example/javaprojektsystemrezerwacjihotelowej/controller/ReservationController.java
+++ src/main/java/org/example/javaprojektsystemrezerwacjihotelowej/controller/ReservationController.java
@@ -2,6 +2,10 @@ package org.example.javaprojektsystemrezerwacjihotelowej.controller;
 
 import org.example.javaprojektsystemrezerwacjihotelowej.entity.Reservation;
 import org.example.javaprojektsystemrezerwacjihotelowej.repository.ReservationsRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/org/example/javaprojektsystemrezerwacjihotelowej/controller/RoomController.java
+++ src/main/java/org/example/javaprojektsystemrezerwacjihotelowej/controller/RoomController.java
@@ -2,6 +2,10 @@ package org.example.javaprojektsystemrezerwacjihotelowej.controller;
 
 import org.example.javaprojektsystemrezerwacjihotelowej.entity.Room;
 import org.example.javaprojektsystemrezerwacjihotelowej.repository.RoomsRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
